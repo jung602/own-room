98aab8e559c8691b51c3a4caf8d493824f0176fb
jung602 <kuhaku05@naver.com>
2025-11-05 19:21:42 +0900

    basepath update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProd ? '/own-room' : '',
+  assetPrefix: isProd ? '/own-room' : '',
+  output: 'export',
 };
 
 export default nextConfig;
